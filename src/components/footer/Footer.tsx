14274581499f42f5b5c971c958eac6e637a6198b
UNNEW-main <1970005826c@gmail.com>
2022-02-28 13:06:16 +0900

    [ADD] Footer List UI 구현

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,13 +3,21 @@ import { Awards, CopyRight, FooterList } from './index';
 export function Footer() {
   return (
     <Container>
+      <FooterList />
       <Awards />
       <CopyRight />
-      <FooterList />
     </Container>
   );
 }
 
 const Container = styled.div`
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #2c2a29;
+  color: #fff;
+  padding-top: 32px;
+  padding-bottom: 35px;
 `;
--- src/components/footer/FooterList.tsx
+++ src/components/footer/FooterList.tsx
@@ -1,5 +1,61 @@
 import styled from 'styled-components';
 
 export function FooterList() {
-  return <div>FooterList</div>;
+  return (
+    <Container>
+      <UlList>
+        <LiHeader>COMPANY</LiHeader>
+        <LiItem>한눈에 보기</LiItem>
+        <LiItem>스타벅스 사명</LiItem>
+        <LiItem>스타벅스 소개</LiItem>
+        <LiItem>국내 뉴스룸</LiItem>
+        <LiItem>세계의 스타벅스</LiItem>
+        <LiItem>글로벌 뉴스룸</LiItem>
+      </UlList>
+      <UlList>
+        <LiHeader>CORPORATE SALES</LiHeader>
+        <LiItem>단체 및 기업 구매 안내</LiItem>
+      </UlList>
+      <UlList>
+        <LiHeader>PARTNERSHIP</LiHeader>
+        <LiItem>신규 입점 제의</LiItem>
+        <LiItem>협력 고객사 등록신청</LiItem>
+      </UlList>
+      <UlList>
+        <LiHeader>ONLINE COMMUNITY</LiHeader>
+        <LiItem>페이스북</LiItem>
+        <LiItem>트위터</LiItem>
+        <LiItem>유튜브</LiItem>
+        <LiItem>인스타그램</LiItem>
+      </UlList>
+      <UlList>
+        <LiHeader>RECRUIT</LiHeader>
+        <LiItem>채용 소개</LiItem>
+        <LiItem>채용 지원하기</LiItem>
+      </UlList>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  width: 80%;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+const UlList = styled.div`
+  width: 20%;
+`;
+
+const LiHeader = styled.div`
+  font-size: 14px;
+  margin-bottom: 15px;
+  font-weight: bold;
+`;
+const LiItem = styled.div`
+  font-size: 12px;
+  font-weight: bold;
+
+  margin-bottom: 7px;
+`;
