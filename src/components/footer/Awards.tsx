d64e1d4cdae53cf769c754eb23a3693cb15b72c3
UNNEW-main <1970005826c@gmail.com>
2022-02-28 14:52:28 +0900

    [ADD] Awards, CopyRight UI 구현완료

--- src/components/footer/Awards.tsx
+++ src/components/footer/Awards.tsx
@@ -5,19 +5,45 @@ export function Awards() {
     <Container>
       <Wrapper>
         <List>
-          <Item></Item>
-          <Item></Item>
-          <Item></Item>
+          <Item>
+            <Img src="https://image.istarbucks.co.kr/common/img/footer/t_footer_award23.jpg" />
+          </Item>
+          <Item>
+            <Img src="https://image.istarbucks.co.kr/common/img/footer/t_footer_award24.jpg" />
+          </Item>
+          <Item>
+            <Img src="https://image.istarbucks.co.kr/common/img/footer/t_footer_award25.jpg" />
+          </Item>
         </List>
       </Wrapper>
     </Container>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  margin-top: 40px;
+  padding-top: 20px;
+  padding-bottom: 40px;
+  background-color: #282828;
+`;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  margin: auto;
+`;
 
-const List = styled.div``;
+const List = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+`;
 
 const Item = styled.div``;
+const Img = styled.img`
+  width: 210px;
+`;
--- src/components/footer/CopyRight.tsx
+++ src/components/footer/CopyRight.tsx
@@ -1,49 +1,97 @@
 import styled from 'styled-components';
-
+import { Button } from '../Atoms';
 export function CopyRight() {
   return (
     <Container>
-      <Wrapper>
-        <List>
-          <Item>
-            <Img src="https://image.istarbucks.co.kr/common/img/footer/t_footer_award23.jpg" />
-          </Item>
-          <Item>
-            <Img src="https://image.istarbucks.co.kr/common/img/footer/t_footer_award24.jpg" />
-          </Item>
-          <Item>
-            <Img src="https://image.istarbucks.co.kr/common/img/footer/t_footer_award25.jpg" />
-          </Item>
-        </List>
-      </Wrapper>
+      <CopyRightList>
+        <CopyRightItem>개인정보처리방침</CopyRightItem>
+        <CopyRightItem>영상정보처리기기 운영관리 방침</CopyRightItem>
+        <CopyRightItem>홈페이지 이용약관</CopyRightItem>
+        <CopyRightItem>위치정보 이용약관</CopyRightItem>
+        <CopyRightItem>스타벅스 카드 이용약관</CopyRightItem>
+        <CopyRightItem>비회원 이용약관</CopyRightItem>
+        <CopyRightItem>My DT Pass 서비스 이용약관</CopyRightItem>
+        <CopyRightItem>윤리경영 핫라인</CopyRightItem>
+      </CopyRightList>
+      <ButtonList>
+        <ButtonWrapper>
+          <Button fontColor="white" borderColor="white" padding="14px">
+            찾아오시는 길
+          </Button>
+        </ButtonWrapper>
+        <ButtonWrapper>
+          <Button fontColor="white" borderColor="white" padding="16px">
+            신규입점제의
+          </Button>
+        </ButtonWrapper>
+        <ButtonWrapper>
+          <Button fontColor="white" borderColor="white" padding="27px">
+            사이트 맵
+          </Button>
+        </ButtonWrapper>
+      </ButtonList>
+      <CopyMenu>
+        <CopyItem>사업자등록번호 : 201-81-21515</CopyItem>
+        <CopyItem>주식회사 에스씨케이컴퍼니 대표이사 : 송 데이비드 호섭</CopyItem>
+        <CopyItem>TEL : 1522-3232</CopyItem>
+        <CopyItem>개인정보 책임자 : 하익성</CopyItem>
+      </CopyMenu>
+      <Copy>ⓒ 2022 Starbucks Coffee Company. All Rights Reserved.</Copy>
     </Container>
   );
 }
 
 const Container = styled.div`
-  width: 100%;
+  width: 80%;
   margin-top: 40px;
-  padding-top: 20px;
   padding-bottom: 40px;
-  background-color: #282828;
 `;
 
-const Wrapper = styled.div`
+const CopyRightList = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 80%;
-  margin: auto;
+`;
+const CopyRightItem = styled.div`
+  padding: 0 12px;
+  font-size: 12px;
+  font-weight: bold;
+  border-left: 1px solid #393939;
+  color: #ccc;
+  :first-child {
+    color: #00b050 !important;
+    border-left: 0px;
+  }
 `;
 
-const List = styled.div`
+const ButtonList = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 80%;
+  margin-top: 10px;
+`;
+const ButtonWrapper = styled.div`
+  margin: 0 5px;
 `;
 
-const Item = styled.div``;
-const Img = styled.img`
-  width: 210px;
+const CopyMenu = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+const CopyItem = styled.div`
+  margin: 0 8px;
+  font-size: 12px;
+  color: #999;
+`;
+
+const Copy = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  font-size: 12px;
+  color: #999;
 `;
