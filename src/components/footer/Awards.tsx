2e90538221beac7e22945ba061ab2ca13194e3d5
UNNEW-main <1970005826c@gmail.com>
2022-02-28 13:40:01 +0900

    [ADD] Awards UI 구현

--- src/components/footer/Awards.tsx
+++ src/components/footer/Awards.tsx
@@ -1,5 +1,23 @@
 import styled from 'styled-components';
 
 export function Awards() {
-  return <div>Awards</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <List>
+          <Item></Item>
+          <Item></Item>
+          <Item></Item>
+        </List>
+      </Wrapper>
+    </Container>
+  );
 }
+
+const Container = styled.div``;
+
+const Wrapper = styled.div``;
+
+const List = styled.div``;
+
+const Item = styled.div``;
--- src/components/footer/CopyRight.tsx
+++ src/components/footer/CopyRight.tsx
@@ -1,5 +1,49 @@
 import styled from 'styled-components';
 
 export function CopyRight() {
-  return <div>CopyRight</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <List>
+          <Item>
+            <Img src="https://image.istarbucks.co.kr/common/img/footer/t_footer_award23.jpg" />
+          </Item>
+          <Item>
+            <Img src="https://image.istarbucks.co.kr/common/img/footer/t_footer_award24.jpg" />
+          </Item>
+          <Item>
+            <Img src="https://image.istarbucks.co.kr/common/img/footer/t_footer_award25.jpg" />
+          </Item>
+        </List>
+      </Wrapper>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  width: 100%;
+  margin-top: 40px;
+  padding-top: 20px;
+  padding-bottom: 40px;
+  background-color: #282828;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  margin: auto;
+`;
+
+const List = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+`;
+
+const Item = styled.div``;
+const Img = styled.img`
+  width: 210px;
+`;
